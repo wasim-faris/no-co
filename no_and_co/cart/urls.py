53d80539844f69d23277ecfc427d33a28be51cf6
Waseem Faris <waseemfaris179@gmail.com>
2026-04-04 09:36:47 +0530

    cart logic added

--- no_and_co/cart/urls.py
+++ no_and_co/cart/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('cart/', views.cart_view, name='cart'),
-    path("add-to-cart/<int:id>/", views.add_to_cart, name="add_to_cart")
+    path("add-to-cart/<int:variant_id>/", views.add_to_cart, name="add-to-cart")
 ]
--- no_and_co/cart/views.py
+++ no_and_co/cart/views.py
@@ -1,8 +1,44 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render,redirect
+from django.shortcuts import get_object_or_404
+from products.models import Variant
+from .models import Cart
+from django.contrib import messages
 def cart_view(request):
     return render(request, 'cart.html')
 
-def add_to_cart(request):
-    pass
+def add_to_cart(request, variant_id):
+    if not request.session.session_key:
+        request.session.create()
+
+    session_key = request.session.session_key
+
+    if request.user.is_authenticated:
+        user = request.user
+        session = None
+    else:
+        user = None
+        session = session_key
+
+    variant = get_object_or_404(Variant, id=variant_id)
+
+    cart_item = Cart.objects.filter(
+        user=user,
+        session_key=session,
+        variant=variant
+    ).first()
+
+
+    if cart_item:
+        cart_item.quantity+=1
+        cart_item.save()
+    else:
+        Cart.objects.create(
+            variant = variant,
+            user=user,
+            session_key = session,
+            quantity = 1,
+            price = variant.price
+        )
+
+    messages.success(request, "Product Added to cart")
+    return redirect(request.META.get('HTTP_REFERER'))
