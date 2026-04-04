1f6906711a8d0bd29dd80917fe1accce2106a2c3
Waseem Faris <waseemfaris179@gmail.com>
2026-04-04 16:49:20 +0530

    small errror is there to fix

--- no_and_co/cart/views.py
+++ no_and_co/cart/views.py
@@ -24,15 +24,15 @@ def cart_view(request):
             if request.headers.get("x-requested-with") == "XMLHttpRequest":
                 user = request.user if request.user.is_authenticated else None
                 session = None if request.user.is_authenticated else request.session.session_key
-                
+
                 order_total = Cart.objects.filter(
                     user=user, session_key=session
                 ).aggregate(total=Sum(F("price") * F("quantity")))["total"] or 0
-                
+
                 delivery_fee = 0 if order_total < 1999 else 149
                 full_total = delivery_fee + order_total
                 item_total = cart_obj.price * cart_obj.quantity
-                
+
                 return JsonResponse({
                     "quantity": cart_obj.quantity,
                     "item_total": item_total,
@@ -61,7 +61,7 @@ def cart_view(request):
     )
     for item in cart_items:
         item.total_price = item.price * item.quantity
-        
+
         order_total = Cart.objects.filter(
         user=user,
         session_key = session
@@ -98,6 +98,7 @@ def add_to_cart(request, variant_id):
 
     variant = get_object_or_404(Variant, id=variant_id)
 
+
     cart_item = Cart.objects.filter(
         user=user,
         session_key=session,
@@ -118,11 +119,11 @@ def add_to_cart(request, variant_id):
         )
 
     messages.success(request, "Product Added to cart")
-    
+
     if request.headers.get("x-requested-with") == "XMLHttpRequest":
         cart_count = Cart.objects.filter(user=user).count() if user else Cart.objects.filter(session_key=session).count()
         return JsonResponse({"success": True, "cart_count": cart_count})
-        
+
     return redirect(request.META.get('HTTP_REFERER', '/'))
 
 def delete_cart_item(request):
@@ -140,15 +141,15 @@ def delete_cart_item(request):
             if request.headers.get("x-requested-with") == "XMLHttpRequest":
                 user = request.user if request.user.is_authenticated else None
                 session = None if request.user.is_authenticated else request.session.session_key
-                
+
                 order_total = Cart.objects.filter(
                     user=user, session_key=session
                 ).aggregate(total=Sum(F("price") * F("quantity")))["total"] or 0
-                
+
                 delivery_fee = 0 if order_total < 1999 else 149
                 full_total = delivery_fee + order_total
                 cart_count = Cart.objects.filter(user=user).count() if user else Cart.objects.filter(session_key=session).count()
-                
+
                 return JsonResponse({
                     "success": True,
                     "order_total": order_total,
