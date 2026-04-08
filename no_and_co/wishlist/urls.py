de094460ead924407a2bca6e45685a3bb308fb86
Waseem Faris <waseemfaris179@gmail.com>
2026-04-08 15:49:21 +0530

    added logic in wishlist add to cart

--- no_and_co/wishlist/urls.py
+++ no_and_co/wishlist/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('wishlist/', views.wishlist, name='wishlist'),
-    path("wishlist-toggle/",views.wishlist_toggle, name="wishlist-toggle")
+    path("wishlist-toggle/",views.wishlist_toggle, name="wishlist-toggle"),
+    path("wishlist-add-to-cart/", views.wishlist_add_to_cart)
 ]
--- no_and_co/wishlist/views.py
+++ no_and_co/wishlist/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.db.models import Prefetch
+from cart.models import Cart
 def wishlist(request):
 
     if not request.session.session_key:
@@ -82,3 +83,93 @@ def wishlist_toggle(request):
         return JsonResponse({
         "status": "added"
         })
+
+def wishlist_add_to_cart(request):
+    data = json.loads(request.body)
+    variant_id = data.get("variant_id")
+
+    variant = get_object_or_404(Variant, id=variant_id)
+
+    if variant.stock <=0:
+        return JsonResponse({
+            "error":"out of stock"
+        })
+
+    if not request.session.session_key:
+        request.session.create()
+
+    if request.user.is_authenticated:
+        cart_item = Cart.objects.filter(user = request.user, variant=variant)
+        if cart_item.exists():
+            item = cart_item.first()
+            if item.quantity >= variant.stock:
+                return JsonResponse({
+                    "error":"out of stock"
+                })
+            item.quantity+=1
+            item.save()
+        else:
+            Cart.objects.create(
+                user = request.user,
+                variant=variant,
+                price = variant.price
+            )
+    else:
+        cart_item = Cart.objects.filter(session_key = request.session.session_key , variant = variant)
+        if cart_item.exists():
+            item = cart_item.first()
+            if item.quantity >= variant.stock:
+                return JsonResponse({
+                    "error":"out of stock"
+                })
+            item.quantity +=1
+            item.save()
+        else:
+            Cart.objects.create(
+                session_key = request.session.session_key,
+                variant = variant,
+                price = variant.price
+            )
+    return JsonResponse({
+    "success": True
+        })
+
+#     else:
+#         user = None
+#         session_key = request.session.session_key
+
+#     if request.user.is_authenticated:
+#         cart_item = Cart.objects.filter(user = user , variant=variant)
+#         if cart_item.exists():
+#             item = cart_item.first()
+#             if item.quantity >= variant.stock:
+#                 return JsonResponse({
+#                 "error": "No more stock available"
+#                         })
+#             item.quantity +=1
+#             item.save()
+#         else:
+#             Cart.objects.create(
+#                 user = user,
+#                 price = variant.price,
+#                 variant = variant
+#             )
+#     else:
+#         cart_item = Cart.objects.filter(session_key = session_key, variant = variant)
+#         if cart_item.exists():
+#             item = cart_item.first()
+#             if item.quantity >= variant.stock:
+#                 return JsonResponse({
+#                 "error": "No more stock available"
+#                 })
+#             item.quantity +=1
+#             item.save()
+#         else:
+#             Cart.objects.create(
+#                 session_key = session_key,
+#                 variant = variant,
+#                 price = variant.price
+#             )
+#     return JsonResponse({
+#     "success": True
+# })
