d630ba35d33b790993eca6c4cec5565c32622b0e
Waseem Faris <waseemfaris179@gmail.com>
2026-04-14 15:51:32 +0530

    added logic in the admin side now add the real data in the frontend

--- no_and_co/order_management/urls.py
+++ no_and_co/order_management/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('admin-orders/', views.orders_list, name='admin-orders-list'),
     path('admin-orders/<str:order_id>/', views.order_detail, name='admin-order-detail'),
     path('admin-inventory/', views.inventory_list, name='admin-inventory-list'),
-    
+    path('admin-update-order-status/<int:order_id>/', views.admin_update_order_status, name='admin-update-order-status')
+
 ]
