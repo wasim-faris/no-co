bdee149f0dd2a0e0833c742133f8c1e04ce75768
Waseem Faris <waseemfaris179@gmail.com>
2026-04-18 00:52:26 +0530

    url changes to ladies first

--- no_and_co/core/urls.py
+++ no_and_co/core/urls.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 urlpatterns = [
-    path("", views.home, name='home'),
+    path("", views.ladies, name='home'),
+    path("mens/", views.home ,name="mens"),
     path("ladies/", views.ladies ,name="ladies"),
     path("kids/", views.kids, name="kids"),
     path("product-details/<int:id>/", views.product_details , name="product-details"),
