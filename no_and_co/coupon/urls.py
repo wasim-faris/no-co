28cba046520c4e54db0a4e4c5cf97a481279d08c
Waseem Faris <waseemfaris179@gmail.com>
2026-04-20 18:12:09 +0530

    added edit coupon logic

--- no_and_co/coupon/urls.py
+++ no_and_co/coupon/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('admin-coupon-management/', views.admin_coupons, name='admin-coupons'),
-    path("add-coupon/", views.add_coupons , name="add-coupon")
+    path("add-coupon/", views.add_coupon , name="add-coupon"),
+    path("edit-coupon/", views.edit_coupon, name="edit-coupon")
 ]
--- no_and_co/coupon/views.py
+++ no_and_co/coupon/views.py
@@ -1,7 +1,3 @@
-from contextlib import redirect_stderr
-from email import message
-from pyexpat import model
-from winreg import REG_QWORD
 from django.contrib import messages
 from django.shortcuts import render,redirect
 from admin_dashboard.decorators import admin_required
@@ -17,12 +13,12 @@ def admin_coupons(request):
     paginator = Paginator(coupons, 4)
     page_number = request.GET.get("page")
     page_obj = paginator.get_page(page_number)
-    
+
     return render(request, "coupon/admin_coupons.html", {
         "page_obj": page_obj
     })
 
-def add_coupons(request):
+def add_coupon(request):
     if request.method == "POST":
         print(dict(request.POST))
         code = request.POST.get("code")
@@ -55,3 +51,41 @@ def add_coupons(request):
         )
 
         return redirect("admin-coupons")
+
+def edit_coupon(request):
+     if request.method == "POST":
+        print(dict(request.POST))
+        coupon_id = request.POST.get("coupon_id")
+        code = request.POST.get("code")
+        discount_value = request.POST.get("discount_value")
+        discount_type = request.POST.get("discount_type")
+        min_purchase = request.POST.get("min_purchase")
+        max_discount = request.POST.get("max_discount")
+        usage_limit_per_user = request.POST.get("usage_limit_per_user")
+        total_usage_limit = request.POST.get("total_usage_limit")
+        start_date = request.POST.get("start_date")
+        end_date = request.POST.get("end_date")
+        is_active = request.POST.get("is_active") == "true"
+
+        try:
+            coupon_obj = Coupon.objects.get(id=coupon_id)
+            coupon_obj.code = code
+            coupon_obj.discount_type = discount_type
+            coupon_obj.discount_value = discount_value
+            coupon_obj.min_purchase = min_purchase
+            coupon_obj.max_discount = max_discount
+            coupon_obj.usage_limit_per_user = usage_limit_per_user
+            coupon_obj.total_usage_limit = total_usage_limit
+            coupon_obj.start_date = start_date
+            coupon_obj.end_date = end_date
+            coupon_obj.is_active = is_active
+            coupon_obj.save()
+
+            messages.success(request, "coupon edited succesfully")
+            return redirect("admin-coupons")
+
+        except Coupon.DoesNotExist:
+            messages.error(
+                request, "coupon not found"
+            )
+            return redirect("checkout")
